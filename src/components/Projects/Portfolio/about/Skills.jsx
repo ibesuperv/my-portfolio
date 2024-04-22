c355ef60d6866707f1522838fe697961dd5afddc
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-04-22 15:40:25 +0530

    Update Skills.jsx

--- src/components/Projects/Portfolio/about/Skills.jsx
+++ src/components/Projects/Portfolio/about/Skills.jsx
@@ -7,7 +7,11 @@ function Skills() {
         
           <span className='text-yellow-400'>&emsp;. Skills {"{"} </span><br/>
           &emsp;&emsp;&emsp;&emsp;<span className='text-cyan-400'>& techninology-based-skills: {"{"}</span><br/>
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--front end development<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>-html css jst<span className='text-pink-600'>)</span> ;<br/>
+      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--React js<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--Mysql Mongodb<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--SpringBoot <span className='text-pink-600'>)</span> ;<br/>
+          
           &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill2 </span> : <span className='text-pink-600'> var(</span>--technical skills<span className='text-pink-600'>)</span> ;<br/>
           &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill3 </span> : <span className='text-pink-600'> var(</span>--computer skills<span className='text-pink-600'>)</span> ;<br/>
           &emsp;&emsp;&emsp;&emsp;<span className="text-cyan-400">{"}"}</span><br/>
