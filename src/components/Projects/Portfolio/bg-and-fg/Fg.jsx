4ded66ece03582b87cdff8f47a1bf474b508d091
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-04-22 15:24:43 +0530

    Update Fg.jsx

--- src/components/Projects/Portfolio/bg-and-fg/Fg.jsx
+++ src/components/Projects/Portfolio/bg-and-fg/Fg.jsx
@@ -25,8 +25,8 @@ function Fg() {
     <div ref={ref} className="absolute bg-transparent cursor-none z-[3] w-full h-full top-0 left-0">
       <div className="w-full py-10 absolute top-[5%] flex justify-center text-center cursor-none text-zinc-500 text-xs font-semibold">
         <ul>
-          <li><Link className='underline cursor-none' to="/country">countries app</Link></li>
-          <li><Link className='underline cursor-none' to="/notes">notes app</Link></li>
+          <li><Link className='underline decoration-red-400 cursor-none' to="/country">countries app</Link></li>
+          <li><Link className='underline decoration-red-400 cursor-none' to="/notes">notes app</Link></li>
         </ul>
       </div>
       {showCards && <Cards reference={ref} />}
