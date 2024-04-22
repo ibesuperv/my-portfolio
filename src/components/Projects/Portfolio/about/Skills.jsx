b945abf40d4a66bae2341903d8c8c61d93a293b0
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-04-22 15:47:42 +0530

    Update Skills.jsx

--- src/components/Projects/Portfolio/about/Skills.jsx
+++ src/components/Projects/Portfolio/about/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
         
           <span className='text-yellow-400'>&emsp;. Skills {"{"} </span><br/>
           &emsp;&emsp;&emsp;&emsp;<span className='text-cyan-400'>& techninology-based-skills: {"{"}</span><br/>
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>-html css jst<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>-html css js<span className='text-pink-600'>)</span> ;<br/>
       &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--React js<span className='text-pink-600'>)</span> ;<br/>
           &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--Mysql Mongodb<span className='text-pink-600'>)</span> ;<br/>
           &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--SpringBoot <span className='text-pink-600'>)</span> ;<br/>
