e3fb006279093aa8b9c2a8749222cd4c2fa3cd95
ibesuperv <varunb5725@gmail.com>
2024-05-03 19:10:56 +0530

    new updated code

--- src/components/Projects/Country/CountriesHeader.jsx
+++ src/components/Projects/Country/CountriesHeader.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import CountriesCard from './CountriesCard';
-
+import DotLoader from 'react-spinners/DotLoader'; 
 
 function CountriesHeader() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(true); 
 
-  useEffect(()=>{
-    document.title="Countries";
-  })
+  useEffect(() => {
+    document.title = "Countries";
+  }, []);
 
   useEffect(() => {
     const getCountries = async () => {
       try {
         const res = await fetch('https://restcountries.com/v3.1/all');
         let data = await res.json();
-        // Sort countries alphabetically based on name.common
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
         setCountries(data);
+        setLoading(false); 
       } catch (error) {
         console.log(error);
       }
@@ -42,34 +43,36 @@ function CountriesHeader() {
 
   return (
     <>
-      {!countries ? (
-        <h1 className='text-white font-bold uppercase flex items-center justify-center text-center h-screen text-4xl'>Loading</h1>
-      ) : (
-        <section className='main-c-holder'>
-          <div className="search">
-            <form autoComplete='off' onSubmit={handleSearch} className='searchform'>
-              <input
-                type="text"
-                name="search"
-                value={search}
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                }}
-                id="search"
-                placeholder='Search the Country'
-                className='searchbar'
-                required
-              />
-            </form>
-          </div>
+      <section className='main-c-holder'>
+        <div className="search">
+          <form autoComplete='off' onSubmit={handleSearch} className='searchform'>
+            <input
+              type="text"
+              name="search"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              id="search"
+              placeholder='Search the Country'
+              className='searchbar'
+              required
+            />
+          </form>
+        </div>
 
-          <div className="cs-cards">
-            {countries.map((country) => (
+        <div className="cs-cards">
+          {loading ? ( 
+            <div className='spinner'>
+              <DotLoader color='#fff' size={100} />
+            </div>
+          ) : (
+            countries.map((country) => (
               <CountriesCard key={country.name.common} {...country} />
-            ))}
-          </div>
-        </section>
-      )}
+            ))
+          )}
+        </div>
+      </section>
     </>
   );
 }
