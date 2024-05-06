51799c324b9e3a1d55bbebb6c52fd27cb287c270
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-06 11:32:13 +0530

    Update Name.jsx

--- src/components/Projects/Portfolio/Name.jsx
+++ src/components/Projects/Portfolio/Name.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import "./portfolio.css"
 
+
+function Name() {
+  return (
+    <div id='name' className="name">
+    <h1 className='imptext '>
+      <span>I</span>M<span>POSSIBLE</span>
+    </h1>
+    </div>
+  )
+}
+
+export default Name
