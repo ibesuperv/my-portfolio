13424806806221313a75f615ba1838c3b5a8f139
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-11 20:19:28 +0530

    Update Name.jsx

--- src/components/Projects/Portfolio/Name.jsx
+++ src/components/Projects/Portfolio/Name.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import "./portfolio.css"
-
+import React from 'react';
+import "./portfolio.css";
 
 function Name() {
+  const style1 = {
+    color: 'white',
+    fontSize: window.innerWidth < 430 ? '1rem' : '1.8rem', 
+  };
+
+  const style2 = {
+    fontSize: window.innerWidth < 430 ? '0.8rem' : '1rem',
+    color: 'white',
+    textAlign: 'center',
+  };
+
   return (
     <div id='name'>
       <div className="name">
-    <p className="intro" >
-    Hi There 👋 ! This is 
-    </p>
-      <p className="varun" >
-      Varun B 
-      </p>
-        </div>
+        <p className="intro" style={style2}>
+          Hi There 👋 ! This is 
+        </p>
+        <p className="varun" style={style1}>
+          Varun B 
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Name
+export default Name;
