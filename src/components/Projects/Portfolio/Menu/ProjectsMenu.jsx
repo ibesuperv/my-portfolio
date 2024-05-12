5ef71baaa77045aa15cdfa0d3e6b13bd7b3b7799
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-12 07:54:16 +0530

    Update ProjectsMenu.jsx

--- src/components/Projects/Portfolio/Menu/ProjectsMenu.jsx
+++ src/components/Projects/Portfolio/Menu/ProjectsMenu.jsx
@@ -11,12 +11,12 @@ const ProjectsMenu = () => {
       </h3>
       <div className="wrap">
         <button
-          className="button relative h-30px cursor-pointer nav-button"
+          className="button relative h-30px cursor-pointer text-white nav-button"
           type="button"
           aria-label="Toggle Navigation"
         >
           <span className="span menu label--nav-closed">
-            <Link className="text-white" to="/">HOME</Link>
+            <Link to="/">HOME</Link>
           </span>
         </button>
       </div>
