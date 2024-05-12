ecd1caa98d56d7d0ddf544a80e160729d215d68a
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-12 07:16:09 +0530

    Update Skills.jsx

--- src/components/Projects/Portfolio/about/Skills.jsx
+++ src/components/Projects/Portfolio/about/Skills.jsx
@@ -8,16 +8,15 @@ function Skills() {
           <span className='text-yellow-400'>&emsp;. Skills {"{"} </span><br/>
           &emsp;&emsp;&emsp;&emsp;<span className='text-cyan-400'>& techninology-based-skills: {"{"}</span><br/>
           &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>-html css js<span className='text-pink-600'>)</span> ;<br/>
-      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--React js<span className='text-pink-600'>)</span> ;<br/>
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--Mysql Mongodb<span className='text-pink-600'>)</span> ;<br/>
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill1 </span> : <span className='text-pink-600'> var(</span>--SpringBoot <span className='text-pink-600'>)</span> ;<br/>
-          
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill2 </span> : <span className='text-pink-600'> var(</span>--technical skills<span className='text-pink-600'>)</span> ;<br/>
-          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill3 </span> : <span className='text-pink-600'> var(</span>--computer skills<span className='text-pink-600'>)</span> ;<br/>
+      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill2 </span> : <span className='text-pink-600'> var(</span>--React js<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill3 </span> : <span className='text-pink-600'> var(</span>--Mysql Mongodb<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill4 </span> : <span className='text-pink-600'> var(</span>--SpringBoot <span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill5 </span> : <span className='text-pink-600'> var(</span>--technical skills<span className='text-pink-600'>)</span> ;<br/>
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>skill6 </span> : <span className='text-pink-600'> var(</span>--tailwind CSS<span className='text-pink-600'>)</span> ;<br/>
           &emsp;&emsp;&emsp;&emsp;<span className="text-cyan-400">{"}"}</span><br/>
           &emsp;&emsp;&emsp;&emsp; <span className='text-cyan-400'>& physical-mental-skills {"{"}</span><br/>
             &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>quickLearner </span> :  <span className='text-red-400'>true </span>;<br/>
-            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>communication </span> :  <span className='text-pink-600'>not-defined</span> ;<br/>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>communication </span> :  <span className='text-pink-600'>true</span> ;<br/>
             &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>critical-thinking </span>:  <span className='text-red-400'>true </span>;<br/>
             &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span className='text-emerald-600'>fast-decision-making </span> :   <span className='text-red-400'>true </span>;<br/>
             &emsp;&emsp;&emsp;&emsp;<span className="text-cyan-400">{"}"}</span><br/>
