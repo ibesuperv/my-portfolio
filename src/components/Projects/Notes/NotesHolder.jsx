5f0f77eabc35af9284a8cdc2878676965139f171
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-12 08:32:16 +0530

    Update NotesHolder.jsx

--- src/components/Projects/Notes/NotesHolder.jsx
+++ src/components/Projects/Notes/NotesHolder.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from "react";
 import "./notesholder.css";
 import Header from "./Header";
+import ProjectsMenu from "../Portfolio/Menu/ProjectsMenu";
 import Notes from "./Notes";
 
 function NotesHolder() {
+  useEffect(() => {
+    document.title = "Notes";
+  }, []);
 
-  useEffect(
-
-    ()=>{
-      document.title="Notes"
-    }
-  )
   return (
     <div className="h-body">
-    <div className="main">
-      <Header />
-      <Notes />
-    </div>
+      <ProjectsMenu />
+      <div className="main">
+        <Header />
+        <Notes />
+      </div>
     </div>
   );
 }
