17bf083fb76506d95e9c19f862262ce21721e700
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-12 07:27:39 +0530

    Create ProjectsMenu.jsx

---
+++ src/components/Projects/Portfolio/Menu/ProjectsMenu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../portfolio.css";
+
+const ProjectsMenu = () => {
+  return (
+    <header className="header">
+      <h3 className="logo">
+        <span className="text-red-500">i</span>
+        <span className="label--nav-open">besuperv</span>
+      </h3>
+      <div className="wrap">
+        <button
+          className="button relative h-30px cursor-pointer nav-button"
+          type="button"
+          aria-label="Toggle Navigation"
+        >
+          <span className="span menu label--nav-closed">
+            <Link to="/">HOME</Link>
+          </span>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default ProjectsMenu;
