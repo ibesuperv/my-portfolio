c3fc773d3345b897a9059509a7caedca5013c820
varun B <134368522+ibesuperv@users.noreply.github.com>
2024-05-15 16:05:42 +0530

    Update Notes.jsx

--- src/components/Projects/Notes/Notes.jsx
+++ src/components/Projects/Notes/Notes.jsx
@@ -4,46 +4,52 @@ import CreateNote from "./CreateNote";
 import { v4 as uuid } from "uuid";
 import "./Note.css";
 
-function Notes () {
-    const [inputText, setInputText] = useState("")
-    const [notes, setNotes] = useState([])
-    const [editToggle, setEditToggle] = useState(null)
+function Notes() {
+  const [inputText, setInputText] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [editToggle, setEditToggle] = useState(null);
 
-    const editHandler = (id,text) => {
-        setEditToggle(id)
-        setInputText(text)
-    }
-    const saveHandler = () => {
-        if(editToggle) {
-            setNotes(notes.map((note) => (
-                note.id === editToggle ?
-                {...note, text: inputText}
-                : note
-            )))
-        } else {
-            setNotes((prevNotes) => [
-                ...prevNotes, {
-                    id: uuid(),
-                    text: inputText
-                }
-            ])
-        }
-        
-        setInputText("")
-        setEditToggle(null)
+  const editHandler = (id, text) => {
+    setEditToggle(id);
+    setInputText(text);
+  };
+
+  const saveHandler = () => {
+    if (editToggle) {
+      setNotes((prevNotes) =>
+        prevNotes.map((note) =>
+          note.id === editToggle ? { ...note, text: inputText } : note
+        )
+      );
+    } else {
+      setNotes((prevNotes) => [
+        ...prevNotes,
+        {
+          id: uuid(),
+          text: inputText,
+        },
+      ]);
     }
 
-    const deleteHandler = (id) => {
-        const newNotes = notes.filter(n => n.id !== id)
-        setNotes(newNotes)
-    }
+    setInputText("");
+    setEditToggle(null);
+  };
 
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem("Notes"));
-        if (data) {
-          setNotes(data);
-        }
-      }, []);
+  const deleteHandler = (id) => {
+    const newNotes = notes.filter((n) => n.id !== id);
+    setNotes(newNotes);
+  };
+
+  const textHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Notes"));
+    if (data) {
+      setNotes(data);
+    }
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("Notes", JSON.stringify(notes));
@@ -51,33 +57,31 @@ function Notes () {
 
   return (
     <div className="notes">
-      {
-            notes.map((note) => (
-                editToggle === note.id ?
-                <CreateNote 
-                        textHandler={textHandler}
-                    saveHandler={saveHandler}
-            inputText={inputText}
-                        />
-                :
-                <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    editHandler = {editHandler}
-                    deleteHandler= {deleteHandler}
-                >
-                </Note>
-            ))
-        }
-        {
-            editToggle === null ? 
-            <CreateNote 
+      {notes.map((note) =>
+        editToggle === note.id ? (
+          <CreateNote
+            key={note.id}
             textHandler={textHandler}
             saveHandler={saveHandler}
             inputText={inputText}
-        /> : <></>
-        }
+          />
+        ) : (
+          <Note
+            key={note.id}
+            id={note.id}
+            text={note.text}
+            editHandler={editHandler}
+            deleteHandler={deleteHandler}
+          />
+        )
+      )}
+      {editToggle === null && (
+        <CreateNote
+          textHandler={textHandler}
+          saveHandler={saveHandler}
+          inputText={inputText}
+        />
+      )}
     </div>
   );
 }
